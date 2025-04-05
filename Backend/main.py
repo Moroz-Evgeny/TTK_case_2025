5c3215ea8d02ab18fd5a81510a481056df1798c1
Moroz-Evgeny <ya.moroz2020@gmail.com>
2025-04-05 19:50:45 +0300

    deploy test3

--- Backend/main.py
+++ Backend/main.py
@@ -22,7 +22,7 @@ app.include_router(main_api_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http//:0.0.0.0:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"], 
