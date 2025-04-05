61f76c38cd3161d16dd679b3729009cc1ef7d3cf
Moroz-Evgeny <ya.moroz2020@gmail.com>
2025-04-06 01:58:08 +0300

    deploy test cookie8

--- Backend/api/handlers/login.py
+++ Backend/api/handlers/login.py
@@ -34,7 +34,7 @@ async def login_for_acess_token(
   response.set_cookie(
     key="refresh_token",
     value=refresh_token,
-    # httponly=True,  
+    httponly=True,  
     # secure=True,              # Только HTTPS
     samesite=None, 
     max_age=60 * 60 * 24 * 7
@@ -59,7 +59,7 @@ async def refresh_token(request: Request, response: Response, session: AsyncSess
     response.set_cookie(
       key="refresh_token",
       value=refresh_token,
-      # httponly=True,  
+      httponly=True,  
       # secure=True,              # Только HTTPS
       samesite=None, 
       max_age=60 * 60 * 24 * 7
