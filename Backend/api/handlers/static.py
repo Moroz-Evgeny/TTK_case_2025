844aa69dce9dfbea0c7c29304f41952603a99a99
Moroz-Evgeny <ya.moroz2020@gmail.com>
2025-04-06 10:09:52 +0300

    deploy3

--- Backend/api/handlers/static.py
+++ Backend/api/handlers/static.py
@@ -20,7 +20,7 @@ static_router = APIRouter()
 
 @static_router.get("/protected/{file_path:path}")
 async def get_protected_file(file_path: str, user_data: dict = Depends(_get_current_user_from_access_token)):
-    file_path_full = os.path.join("TTK_case_2025", "Backend", "static", "images", file_path)
+    file_path_full = os.path.join("static", "images", file_path)
     if not os.path.exists(file_path_full):
         raise HTTPException(status_code=404, detail="File not found")
     
